015167c2c6462f9c0ceffd51a6664fc1a24f864f
Alon Zakai <azakai@google.com>
2020-05-30 12:58:15 -0700

    more

--- scripts/fuzz_shell.js
+++ scripts/fuzz_shell.js
@@ -169,6 +169,98 @@ var imports = {
     'setTempRet0': function(x) { tempRet0 = x },
     'getTempRet0': function() { return tempRet0 },
     'log_execution': function(x) { console.log('log-execution', x) },
+    'get_i32': function(loc, index, value) {
+      console.log('get_i32 ' + [loc, index, value]);
+      return value;
+    },
+    'get_i64': function(loc, index, low, high) {
+      console.log('get_i64 ' + [loc, index, low, high]);
+      setTempRet0(high);
+      return low;
+    },
+    'get_f32': function(loc, index, value) {
+      console.log('get_f32 ' + [loc, index, value]);
+      return value;
+    },
+    'get_f64': function(loc, index, value) {
+      console.log('get_f64 ' + [loc, index, value]);
+      return value;
+    },
+    'get_anyref': function(loc, index, value) {
+      console.log('get_anyref ' + [loc, index, value]);
+      return value;
+    },
+    'get_exnref': function(loc, index, value) {
+      console.log('get_exnref ' + [loc, index, value]);
+      return value;
+    },
+    'set_i32': function(loc, index, value) {
+      console.log('set_i32 ' + [loc, index, value]);
+      return value;
+    },
+    'set_i64': function(loc, index, low, high) {
+      console.log('set_i64 ' + [loc, index, low, high]);
+      setTempRet0(high);
+      return low;
+    },
+    'set_f32': function(loc, index, value) {
+      console.log('set_f32 ' + [loc, index, value]);
+      return value;
+    },
+    'set_f64': function(loc, index, value) {
+      console.log('set_f64 ' + [loc, index, value]);
+      return value;
+    },
+    'set_anyref': function(loc, index, value) {
+      console.log('set_anyref ' + [loc, index, value]);
+      return value;
+    },
+    'set_exnref': function(loc, index, value) {
+      console.log('set_exnref ' + [loc, index, value]);
+      return value;
+    },
+    'load_ptr': function(loc, bytes, offset, ptr) {
+      console.log('load_ptr ' + [loc, bytes, offset, ptr]);
+      return ptr;
+    },
+    'load_val_i32': function(loc, value) {
+      console.log('load_val_i32 ' + [loc, value]);
+      return value;
+    },
+    'load_val_i64': function(loc, low, high) {
+      console.log('load_val_i64 ' + [loc, low, high]);
+      setTempRet0(high);
+      return low;
+    },
+    'load_val_f32': function(loc, value) {
+      console.log('load_val_f32 ' + [loc, value]);
+      return value;
+    },
+    'load_val_f64': function(loc, value) {
+      console.log('load_val_f64 ' + [loc, value]);
+      return value;
+    },
+    'store_ptr': function(loc, bytes, offset, ptr) {
+      console.log('store_ptr ' + [loc, bytes, offset, ptr]);
+      return ptr;
+    },
+    'store_val_i32': function(loc, value) {
+      console.log('store_val_i32 ' + [loc, value]);
+      return value;
+    },
+    'store_val_i64': function(loc, low, high) {
+      console.log('store_val_i64 ' + [loc, low, high]);
+      setTempRet0(high);
+      return low;
+    },
+    'store_val_f32': function(loc, value) {
+      console.log('store_val_f32 ' + [loc, value]);
+      return value;
+    },
+    'store_val_f64': function(loc, value) {
+      console.log('store_val_f64 ' + [loc, value]);
+      return value;
+    },
   },
 };
 
