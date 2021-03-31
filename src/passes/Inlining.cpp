606cd31440ea82f7a4df9b3790108c0d6cf1eba5
Alon Zakai <azakai@google.com>
2021-03-31 13:03:20 -0700

    format

--- src/passes/Inlining.cpp
+++ src/passes/Inlining.cpp
@@ -45,8 +45,8 @@
 
 #include "ir/cost.h"
 #include "ir/debug.h"
-#include "ir/find_all.h"
 #include "ir/element-utils.h"
+#include "ir/find_all.h"
 #include "ir/literal-utils.h"
 #include "ir/module-utils.h"
 #include "ir/type-updating.h"
