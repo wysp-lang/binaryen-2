4314348cbbe2ccff34661a2c6c84e4caf9ba1182
Alon Zakai <azakai@google.com>
2021-09-30 13:54:05 -0700

    work

--- src/passes/VTableToIndexes.cpp
+++ src/passes/VTableToIndexes.cpp
@@ -14,18 +14,40 @@
  * limitations under the License.
  */
 
-#include <algorithm>
-#include <iomanip>
+//
+// Converts vtables - structs of function references - to use indexes. That is,
+// this replaces function reference fields with i32 fields. Specifically,
+//
+//  (struct (field (ref $functype1)) (field (ref $functype2))
+// =>
+//  (struct (field (ref i32))        (field (ref i32))
+//
+// This also creates a table for each field and populates it with the possible
+// values. Then struct.news are altered to replace references with indexes, and
+// struct.gets are altered to load from the table.
+//
+// Assumptions:
+//  * All function reference fields are to be transformed.
+//  * Such fields must be written to during creation of a vtable instance, and
+//    with a constant ref.func.
+//  * Vtable subtyping is allowed, but not to specialize types of the parent. If
+//    that were done, we'd need to add casts to handle the table no having the
+//    specialized type (it would have the subtype).
+//
+
 #include <ir/module-utils.h>
+#include "ir/subtypes.h" // Needed?
 #include <pass.h>
-#include <support/colors.h>
-#include <wasm-binary.h>
 #include <wasm.h>
+#include <wasm-type.h>
 
 using namespace std;
 
 namespace wasm {
 
+namespace {
+
+#if 0
 struct FunctionInfoScanner
   : public WalkerPass<PostWalker<FunctionInfoScanner>> {
   bool isFunctionParallel() override { return true; }
@@ -41,14 +63,76 @@ struct FunctionInfoScanner
     (*infos)[getFunction()->name].hasLoops = true;
   }
 };
+#endif
 
-// Prints metrics between optimization passes.
 struct VTableToIndexes : public Pass {
   void run(PassRunner* runner, Module* module) override {
-    FunctionInfoScanner().run(runner, module);
+    // First, rewrite the types to replace function references with i32s.
+    std::vector<HeapType> types;
+    std::unordered_map<HeapType, Index> typeIndices;
+    ModuleUtils::collectHeapTypes(*module, types, typeIndices);
+
+    std::unordered_map<HeapType, Index> typeToIndex;
+    for (Index i = 0; i < types.size(); i++) {
+      typeToIndex[types[i]] = i;
+    }
+
+    auto getNewType = [&](Type type, bool isStructField) {
+      if (type.isBasic()) {
+        return type;
+      }
+      if (type.isFunction()) {
+        if (isStructField) {
+          // This is exactly what we are looking to change!
+          return Type::i32;
+        }
+        ..
+      }
+      .. 
+    };
+
+    TypeBuilder typeBuilder(types.size());
+    for (Index i = 0; i < types.size(); i++) {
+      auto type = types[i];
+      if (type.isSignature()) {
+        auto sig = type.getSignature();
+        TypeList newParams, newResults;
+        for (auto t : sig.params) {
+          newParams.push_back(getNewType(t));
+        }
+        for (auto t : sig.results) {
+          newResults.push_back(getNewType(t));
+        }
+        typeBuilder.setHeapType(i, Signature{newParams, newResults};
+      } else if (type.isStruct()) {
+        auto struct_ = type.getStruct();
+        // Start with a copy to get mutability/packing/etc.
+        auto newStruct = struct_;
+        for (auto& field : newStruct.fields) {
+          field.type = getNewType(field.type);
+        }
+        typeBuilder.setHeapType(i, newStruct);
+      } else if (type.isArray()) {
+        auto array = type.getArray();
+        // Start with a copy to get mutability/packing/etc.
+        auto newArray = array;
+        newArray.element = getNewType(newArray.element);
+        }
+        typeBuilder.setHeapType(i, newArray);
+      } else {
+        WASM_UNREACHABLE("bad type");
+      }
+    }
+
+
+
+    //SubTypes subTypes(*module);
+    //FunctionInfoScanner().run(runner, module);
   }
 };
 
-Pass* createVTableToIndexesPass() { return new VTableToIndexes(false); }
+} // anonymous namespace
+
+Pass* createVTableToIndexesPass() { return new VTableToIndexes(); }
 
 } // namespace wasm
