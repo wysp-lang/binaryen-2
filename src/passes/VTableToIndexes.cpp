a13a6764c06d62063a4f22277c941aba21c93f3f
Alon Zakai <azakai@google.com>
2021-10-05 16:35:33 -0700

    wip

--- src/passes/VTableToIndexes.cpp
+++ src/passes/VTableToIndexes.cpp
@@ -38,7 +38,7 @@
 #include <mutex>
 
 #include <ir/module-utils.h>
-//#include "ir/subtypes.h" // Needed?
+#include "ir/type-updating.h"
 #include "ir/utils.h"
 #include <pass.h>
 #include <wasm.h>
@@ -72,109 +72,36 @@ struct VTableToIndexes : public Pass {
   MappingInfo mapping;
 
   void run(PassRunner* runner, Module* module) override {
-std::cout << "map\n";
-    // Create the new types and get a mapping of the old ones to the new.
-    mapOldTypesToNew(*module);
+    // Replace function fields in structs with i32s. This just does the type
+    // changes.
+    updateFieldTypes(*module);
 
+    // Map functions written to struct fields to indexes that can be written to
+    // them instead. This updates the mapping info.
     mapFunctionsToTables(runner, *module);
 
-std::cout << "update\n";
-    // Update all the types to the new ones.
-    updateModule(runner, *module);
+    // Create the tables.
+    createTables(*module);
+
+    // Fix up struct operations that used to be on function references to now
+    // contain indexes.
+    updateStructOperations(*module);
   }
 
-  void mapOldTypesToNew(Module& wasm) {
-    // Collect all the types.
-    std::vector<HeapType> types;
-    std::unordered_map<HeapType, Index> typeIndices;
-    ModuleUtils::collectHeapTypes(wasm, types, typeIndices);
-
-    // We will need to map types to their indexes.
-    std::unordered_map<HeapType, Index> typeToIndex;
-    for (Index i = 0; i < types.size(); i++) {
-      typeToIndex[types[i]] = i;
-    }
-
-    TypeBuilder typeBuilder(types.size());
-
-    // Map an old type to a new type. This is called on the contents of the
-    // temporary heap types, so it basically just needs to map to other temp
-    // heap types.
-    std::function<Type (Type)> getNewType = [&](Type type) {
-      if (type.isBasic()) {
-        return type;
-      }
-      if (type.isRef()) {
-        return typeBuilder.getTempRefType(
-          typeBuilder.getTempHeapType(typeToIndex.at(type.getHeapType())),
-          type.getNullability()
-        );
-      }
-      if (type.isRtt()) {
-        auto rtt = type.getRtt();
-        auto newRtt = rtt;
-        newRtt.heapType = 
-          typeBuilder.getTempHeapType(typeToIndex.at(type.getHeapType()));
-        return typeBuilder.getTempRttType(newRtt);
-      }
-      if (type.isTuple()) {
-        auto& tuple = type.getTuple();
-        auto newTuple = tuple;
-        for (auto& t : newTuple.types) {
-          t = getNewType(t);
+  void updateFieldTypes(Module& wasm) {
+    class TypeRewriter : public GlobalTypeRewriter {
+    public:
+      virtual void modifyStruct(HeapType oldStructType, Struct& struct_) {
+        for (auto& field : struct_.fields) {
+          if (field.type.isFunction()) {
+            // This is exactly what we are looking to change!
+            return Type::i32;
+          }
         }
-        return typeBuilder.getTempTupleType(newTuple);
       }
-      WASM_UNREACHABLE("bad type");
     };
 
-    // Map an old type to a new type, for a struct field. This does the special
-    // operation we are doing here, which is to replace function referencs with
-    // i32s.
-    auto getNewTypeForStruct = [&](Type type) -> Type {
-      if (type.isFunction()) {
-        // This is exactly what we are looking to change!
-        return Type::i32;
-      }
-      return getNewType(type);
-    };
-
-    // Create the temporary heap types.
-    for (Index i = 0; i < types.size(); i++) {
-      auto type = types[i];
-      if (type.isSignature()) {
-        auto sig = type.getSignature();
-        TypeList newParams, newResults;
-        for (auto t : sig.params) {
-          newParams.push_back(getNewType(t));
-        }
-        for (auto t : sig.results) {
-          newResults.push_back(getNewType(t));
-        }
-        typeBuilder.setHeapType(i, Signature(typeBuilder.getTempTupleType(newParams), typeBuilder.getTempTupleType(newResults)));
-      } else if (type.isStruct()) {
-        auto struct_ = type.getStruct();
-        // Start with a copy to get mutability/packing/etc.
-        auto newStruct = struct_;
-        for (auto& field : newStruct.fields) {
-          field.type = getNewTypeForStruct(field.type);
-        }
-        typeBuilder.setHeapType(i, newStruct);
-      } else if (type.isArray()) {
-        auto array = type.getArray();
-        // Start with a copy to get mutability/packing/etc.
-        auto newArray = array;
-        newArray.element.type = getNewType(newArray.element.type);
-        typeBuilder.setHeapType(i, newArray);
-      } else {
-        WASM_UNREACHABLE("bad type");
-      }
-    }
-    auto newTypes = typeBuilder.build();
-
-    for (Index i = 0; i < types.size(); i++) {
-      mapping.oldToNewTypes[types[i]] = newTypes[i];
-    }
+    TypeRewriter(wasm).update();
   }
 
   void mapFunctionsToTables(PassRunner* runner, Module& wasm) {
@@ -245,28 +172,6 @@ std::cout << "update\n";
         auto& tableInfo = mapping.tableFuncIndexes[
 
       }
-
-struct MappingInfo {
-  // Maps old heap types to new ones.
-  std::unordered_map<HeapType, HeapType> oldToNewTypes;
-
-  // For each table, a map of functions in the table to their indexes.
-  std::unordered_map<Name, std::unordered_map<Name, Index>> tableFuncIndexes;
-
-  // Maps each (struct, field index of a function reference) to the table in
-  // which it is declared.
-  std::unordered_map<std::pair<HeapType, Index>, Name> fieldTables;
-
-  // When modifying this data structure in parallel, this mutex must be taken.
-  // Using a mutex is reasonable here since we have few edits to make (only for
-  // struct.news, which are rare).
-  std::mutex mutex;
-};
-
-
-
-
-
     };
 
     CodeScanner updater(mapping);
@@ -341,75 +246,11 @@ std::cout << "type: " << type << "\n";
 
         curr->type = update(curr->type);
       }
-
-      // Generic visitor. Specific things were overridden earlier.
-      void visitExpression(Expression* curr) {
-        // Update the type to the new one.
-        curr->type = update(curr->type);
-
-        // Update any other type fields as well.
-
-#define DELEGATE_ID curr->_id
-
-#define DELEGATE_START(id)                                                     \
-  auto* cast = curr->cast<id>();                                               \
-  WASM_UNUSED(cast);
-
-#define DELEGATE_GET_FIELD(id, name) cast->name
-
-#define DELEGATE_FIELD_TYPE(id, name) \
-  cast->name = update(cast->name);
-
-#define DELEGATE_FIELD_HEAPTYPE(id, name) \
-  cast->name = update(cast->name);
-
-#define DELEGATE_FIELD_SIGNATURE(id, name) \
-  cast->name = update(cast->name);
-
-#define DELEGATE_FIELD_CHILD(id, name)
-#define DELEGATE_FIELD_OPTIONAL_CHILD(id, name)
-#define DELEGATE_FIELD_INT(id, name)
-#define DELEGATE_FIELD_INT_ARRAY(id, name)
-#define DELEGATE_FIELD_LITERAL(id, name)
-#define DELEGATE_FIELD_NAME(id, name)
-#define DELEGATE_FIELD_NAME_VECTOR(id, name)
-#define DELEGATE_FIELD_SCOPE_NAME_DEF(id, name)
-#define DELEGATE_FIELD_SCOPE_NAME_USE(id, name)
-#define DELEGATE_FIELD_SCOPE_NAME_USE_VECTOR(id, name)
-#define DELEGATE_FIELD_ADDRESS(id, name)
-
-#include "wasm-delegations-fields.def"
-      }
     };
 
     CodeUpdater updater(mapping);
     updater.run(runner, &wasm);
     updater.walkModuleCode(&wasm);
-
-    // Propagate types after our changes.
-    ReFinalize().run(runner, &wasm); // XXX th is is wrong. remove header too
-
-    // Update global locations that refer to types.
-    for (auto& table : wasm.tables) {
-      table->type = updater.update(table->type);
-    }
-    for (auto& elementSegment : wasm.elementSegments) {
-      elementSegment->type = updater.update(elementSegment->type);
-    }
-    for (auto& global : wasm.globals) {
-      global->type = updater.update(global->type);
-    }
-    for (auto& func : wasm.functions) {
-      func->type = updater.update(func->type);
-    }
-
-    for (auto& kv : mapping.oldToNewTypes) {
-      auto old = kv.first;
-      auto new_ = kv.second;
-      if (wasm.typeNames.count(old)) {
-        wasm.typeNames[new_] = wasm.typeNames[old];
-      }
-    }
   }
 };
 
