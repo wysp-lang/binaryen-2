ae00cbc547779c738c8e5de636d00fc87e1c4da2
Alon Zakai <azakai@google.com>
2021-10-18 17:01:52 -0700

    work

--- src/passes/UnifyITable.cpp
+++ src/passes/UnifyITable.cpp
@@ -15,24 +15,68 @@
  */
 
 //
-// Converts vtables - structs of function references - to use indexes. That is,
-// this replaces function reference fields with i32 fields. Specifically,
+// Unifies all itables into a single universal global dispatch table. That is,
+// if various classes have itables like this:
 //
-//  (struct (field (ref $functype1)) (field (ref $functype2))
-// =>
-//  (struct (field (ref i32))        (field (ref i32))
+//  (global $foo.itable (array.init $itable
+//    (null) ;; no entry for this category
+//    (struct.new $some.vtable ;; an entry of size 2 for this category
+//      (ref.func $a)
+//      (ref.func $b)
+//    )
+//    ..
+//  ))
 //
-// This also creates a table for each field and populates it with the possible
-// values. Then struct.news are altered to replace references with indexes, and
-// struct.gets are altered to load from the table.
+// Each index refers to what we call a "category", globally. That is, the 0th
+// index in all itables refers to the first category, and so forth. (For
+// example, the first category may be the interface "Serializable"; however,
+// there may also be more than one source interface per category, if the
+// producer emitted them that way.) At each index, an itable has either a null
+// or a vtable that contains function references.
 //
-// Assumptions:
-//  * All function reference fields in structs should be transformed.
-//  * Such fields must be written to during creation of a vtable instance, and
-//    with a constant ref.func, and never written to with struct.set.
-//  * Vtable subtyping is allowed, but not to specialize types of the parent. If
-//    that were done, we'd need to add casts to handle the table no having the
-//    specialized type (it would have the subtype).
+// Then we create a single table to rule them all ("the one table"):
+//
+//  (table $unified-table
+//    (null) (null) ..            ;; as many nulls as that first category needs
+//    (ref.func $a) (ref.func $b) ;; the funcs from the second category (plus
+//                                ;; nulls, to fill the category, see below)
+//  )
+//
+// Fields that were (ref $itable) become i32s, where that value is the offset
+// into the one table, which indicates where that class's info begins. To that
+// we add:
+//
+//  * The itable category offset, which in the old model was the constant
+//    offset in the array.get, and which now becomes a global "category size"
+//    constant. All categories are forced to the same size.
+//  * The vtable offset. This is simply the index in the vtable that the
+//    struct.get used.
+//
+// Assuming the itable category is constant, this means we replace something
+// like
+//
+//  (call_ref
+//   (struct.get from vtable
+//    (ref.cast to vtable
+//     (array.get from itable
+//      (struct.get to get the itable
+//
+// with
+//
+//  (call_indirect
+//   (i32.add with a constant (both itable category and vtable offset)
+//    (struct.get to get the class base in the one table
+//
+// That is, we replace a cast, a struct.get, and an array.get with an add. (We
+// also replace a call_ref with a call_indirect, which may be slightly less
+// efficient, actually, and is worth investigation.)
+//
+// Assumption: The itable field is called $itable, and nothing else has that
+// name; and likewise the itable array is called $itable.
+//
+// This may make DCE less productive, as DCE would need to know about which
+// constant indices are used inside objects (which it does not atm). For that
+// reason this pass makes sense as a final transformation.
 //
 
 #include <ir/module-utils.h>
@@ -50,35 +94,148 @@ namespace wasm {
 
 namespace {
 
+static const Name ITABLE("itable");
+
 struct UnifyITable : public Pass {
   void run(PassRunner* runner, Module* module) override {
-    // Map functions written to struct fields to indexes that can be written to
-    // them instead. This includes creating the tables and fixing up struct
-    // operations to access the tables.
-    mapFunctionsToTables(runner, *module);
+    // Process itable data in globals and compute the layout of the unified
+    // table. This also applies those changes to code.
+    mapFunctionsToTable(runner, *module);
 
-    // Replace function fields in structs with i32s. This just does the type
-    // changes.
+    // Replace $itable function fields in structs with i32s. This just does the
+    // type changes.
     updateFieldTypes(*module);
   }
 
-  void mapFunctionsToTables(PassRunner* runner, Module& wasm) {
+  void mapFunctionsToTable(PassRunner* runner, Module& wasm) {
+    // Create the unified table.
+    auto unifiedTable =
+      Names::getValidTableName(*getModule(), "unified-table");
+    wasm.addTable(Builder::makeTable(unifiedTable, Type::funcref));
+    auto segmentName = Names::getValidElementSegmentName(
+      wasm, unifiedTable.str + std::string("$segment"));
+    auto* segment = wasm.addElementSegment(Builder::makeElementSegment(
+      segmentName,
+      unifiedTable,
+      Builder(*getModule()).makeConst(int32_t(0)),
+      Type::funcref));
+    auto& segmentData = segment->data;
+
+    // Fill in mapping info that describes how we lay out the unified table.
     struct MappingInfo {
-      // Maps each (struct, field index of a function reference) to the table in
-      // which it is declared.
-      std::unordered_map<std::pair<HeapType, Index>, Name> fieldTables;
+      // The size of each category.
+      std::vector<Index> categorySizes;
 
-      struct TableInfo {
-        // The name of the singleton segment for the table.
-        Name segmentName;
+      // The list of itable globals, in the order they appear in the wasm.
+      std::vector<Name> itables;
 
-        // A map of functions in the table to their indexes.
-        std::unordered_map<Name, Index> funcIndexes;
-      };
+      // Maps the names of itable globals in the old model to base offsets in
+      // the unified table in the new model.
+      std::unordered_map<Name, Index> itableToBase;
 
-      // Information for all the tables.
-      std::unordered_map<Name, TableInfo> tableInfos;
+
     } mappingInfo;
+
+    // Find the itable globals.
+    ModuleUtils::iterDefinedGlobals(
+      wasm, [&](Global* global) {
+        // Itables are created with array.init and nothing else.
+        if (!global->init->is<ArrayInit>()) {
+          return;
+        }
+
+        auto type = global->init->type;
+        if (!type.isArray()) {
+          return;
+        }
+        if (wasm.typeNames[type.getHeapType()].name != ITABLE) {
+          return;
+        }
+
+        // This is an itable.
+        mappingInfo.itables.push_back(global->name);
+      });
+    auto numItables = mappingInfo.itables.size();
+
+    // Compute the size of the categories.
+    auto ensureCategorySize = [&](Index index, Index size) {
+      mappingInfo.categorySizes.resize(index + 1);
+      mappingInfo.categorySizes[index] = std::max(mappingInfo.categorySizes[index], size);
+    };
+
+    for (auto itable : mappingInfo.itables) {
+      auto itableGlobal = wasm.getGlobal(itable);
+      auto itableOperands = itableGlobal->init->cast<ArrayInit>()->operands;
+      Index index = 0;
+      for (auto* operand : itableOperands) {
+        // Elements in an itable are either a null or a struct.new.
+        if (operand->is<RefNull>()) {
+          // Just ensure the category exists.
+          ensureCategorySize(index, 0);
+        } else if (auto* new_ = operand->dynCast<StructNew>()) {
+          ensureCategorySize(index, new_->operands.size());
+        } else {
+          WASM_UNREACHABLE("bad array.init operand");
+        }
+        index++;
+      }
+    }
+
+    // The size of an itable in the new layout is the sum of all the category
+    // sizes;
+    Index itableSize = 0;
+    for (auto size : mappingInfo.categorySizes) {
+      itableSize += size;
+    }
+
+    // The unified table's segment's size is now known.
+    segmentData.resize(itableSize * numItables);
+
+    // Now that we know the sizes of the categories, we can generate the layout
+    // of the unified table and fill it out.
+    Builder builder(wasm);
+    Index tableIndex = 0;
+    for (auto itable : mappingInfo.itables) {
+      auto itableGlobal = wasm.getGlobal(itable);
+      auto itableOperands = itableGlobal->init->cast<ArrayInit>()->operands;
+
+      // Pick the base for this itable.
+      mappingInfo.itableToBase[itable] = tableIndex;
+
+      auto categoryIndex = 0;
+      for (auto* operand : itableOperands) {
+        auto categorySize = mappingInfo.categorySizes[categoryIndex];
+        if (operand->is<RefNull>()) {
+          // Fill the entire category with nulls.
+          for (Index i = 0; i < categorySize; i++) {
+            segmentData[tableIndex++] = builder.makeRefNull(HeapType::func);
+          }
+        } else if (auto* new_ = operand->dynCast<StructNew>()) {
+          // Copy in the contents.
+          for (auto* newOperand : new_->operands) {
+            auto* refFunc = newOperand->cast<RefFunc>();
+            segmentData[tableIndex++] = builder.makeRefFunc(refFunc->name, refFunc->type);
+          }
+
+          // Fill the remaining space with nulls.
+          for (Index i = 0; i < categorySize - new_->operands.size(); i++) {
+            segmentData[tableIndex++] = builder.makeRefNull(HeapType::func);
+          }
+        } else {
+          WASM_UNREACHABLE("bad array.init operand");
+        }
+        categoryIndex++;
+      }
+    }
+    
+
+
+
+
+
+
+
+
 
     struct Mapper : public WalkerPass<PostWalker<Mapper>> {
       // Intentionally *not* function parallel, to make it deterministic, and
