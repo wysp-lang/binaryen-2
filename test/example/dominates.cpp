556ed87d4a5f44489d47c2fa23e3097cbd60274a
Alon Zakai <azakai@google.com>
2021-12-02 13:25:55 -0800

    test

--- test/example/dominates.cpp
+++ test/example/dominates.cpp
@@ -37,7 +37,7 @@ struct CFG : public std::vector<std::unique_ptr<BasicBlock>> {
 // and also check the reverse (right, left) combination. If left == right then
 // that must assert as true, the same as unflipped. Otherwise, it must check
 // as the reverse, as domination of different items is antisymmetrical.
-#define CHECK_SYMMETRIC(check, left, right)                                    \
+#define CHECK_TRUE(check, left, right)                                    \
   {                                                                            \
     assert(check(left, right));                                                \
     if (left == right) {                                                       \
@@ -46,6 +46,16 @@ struct CFG : public std::vector<std::unique_ptr<BasicBlock>> {
       assert(!check(right, left));                                             \
     }                                                                          \
   }
+
+// As above, but check that the result is false for both (left, right) and
+// (right, left). This is the case for two blocks where neither domiantes the
+// other.
+#define CHECK_FALSE(check, left, right)                                    \
+  {                                                                            \
+    assert(!check(left, right));                                                \
+    assert(!check(right, left));                                             \
+  }
+
 
 int main() {
   Module temp;
@@ -62,14 +72,14 @@ int main() {
     cfg::DominationChecker<BasicBlock> checker(cfg);
 
     // Things dominate themselves.
-    CHECK_SYMMETRIC(checker.dominates, first, first);
-    CHECK_SYMMETRIC(checker.dominates, second, second);
-    CHECK_SYMMETRIC(checker.dominates, third, third);
+    CHECK_TRUE(checker.dominates, first, first);
+    CHECK_TRUE(checker.dominates, second, second);
+    CHECK_TRUE(checker.dominates, third, third);
 
     // Things dominate those after them.
-    CHECK_SYMMETRIC(checker.dominates, first, second);
-    CHECK_SYMMETRIC(checker.dominates, first, third);
-    CHECK_SYMMETRIC(checker.dominates, second, third);
+    CHECK_TRUE(checker.dominates, first, second);
+    CHECK_TRUE(checker.dominates, first, third);
+    CHECK_TRUE(checker.dominates, second, third);
   }
 
   // entry => next, with items in both.
@@ -86,20 +96,20 @@ int main() {
     cfg::DominationChecker<BasicBlock> checker(cfg);
 
     // Things dominate themselves in all blocks.
-    CHECK_SYMMETRIC(checker.dominates, entryA, entryA);
-    CHECK_SYMMETRIC(checker.dominates, entryB, entryB);
-    CHECK_SYMMETRIC(checker.dominates, nextA, nextA);
-    CHECK_SYMMETRIC(checker.dominates, nextB, nextB);
+    CHECK_TRUE(checker.dominates, entryA, entryA);
+    CHECK_TRUE(checker.dominates, entryB, entryB);
+    CHECK_TRUE(checker.dominates, nextA, nextA);
+    CHECK_TRUE(checker.dominates, nextB, nextB);
 
     // Things dominate things after them in the same block.
-    CHECK_SYMMETRIC(checker.dominates, entryA, entryB);
-    CHECK_SYMMETRIC(checker.dominates, nextA, nextB);
+    CHECK_TRUE(checker.dominates, entryA, entryB);
+    CHECK_TRUE(checker.dominates, nextA, nextB);
 
     // The entry block items dominate items in the next block.
-    CHECK_SYMMETRIC(checker.dominates, entryA, nextA);
-    CHECK_SYMMETRIC(checker.dominates, entryA, nextB);
-    CHECK_SYMMETRIC(checker.dominates, entryB, nextA);
-    CHECK_SYMMETRIC(checker.dominates, entryB, nextB);
+    CHECK_TRUE(checker.dominates, entryA, nextA);
+    CHECK_TRUE(checker.dominates, entryA, nextB);
+    CHECK_TRUE(checker.dominates, entryB, nextA);
+    CHECK_TRUE(checker.dominates, entryB, nextB);
   }
 
   // Domination with an intermediate block, entry => middle => last
@@ -116,9 +126,45 @@ int main() {
 
     cfg::DominationChecker<BasicBlock> checker(cfg);
 
-    CHECK_SYMMETRIC(checker.dominates, entryA, middleA);
-    CHECK_SYMMETRIC(checker.dominates, entryA, lastA);
-    CHECK_SYMMETRIC(checker.dominates, middleA, lastA);
+    CHECK_TRUE(checker.dominates, entryA, middleA);
+    CHECK_TRUE(checker.dominates, entryA, lastA);
+    CHECK_TRUE(checker.dominates, middleA, lastA);
+  }
+
+  // An if:
+  //            b
+  //           / \
+  // entry -> a   d
+  //           \ /
+  //            c
+  {
+    CFG cfg;
+    auto* entry = cfg.add();
+    auto* a = cfg.add();
+    auto* b = cfg.add();
+    auto* c = cfg.add();
+    auto* d = cfg.add();
+    cfg.connect(entry, a);
+    cfg.connect(a, b);
+    cfg.connect(a, c);
+    cfg.connect(b, d);
+    cfg.connect(c, d);
+    auto* aX = a->addItem(builder.makeNop());
+    auto* bX = b->addItem(builder.makeNop());
+    auto* cX = c->addItem(builder.makeNop());
+    auto* dX = d->addItem(builder.makeNop());
+
+    cfg::DominationChecker<BasicBlock> checker(cfg);
+
+    // a dominates all the others.
+    CHECK_TRUE(checker.dominates, aX, bX);
+    CHECK_TRUE(checker.dominates, aX, cX);
+    CHECK_TRUE(checker.dominates, aX, dX);
+
+    // b, c, and d do not dominate amongst each other.
+    CHECK_FALSE(checker.dominates, bX, cX);
+    CHECK_FALSE(checker.dominates, bX, dX);
+    CHECK_FALSE(checker.dominates, cX, dX);
   }
 
   std::cout << "success.\n";
