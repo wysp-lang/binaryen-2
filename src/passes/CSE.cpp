1f18e14d513db5800ac49848154d96990e5d1236
Alon Zakai <azakai@google.com>
2021-12-04 18:54:10 -0800

    fix

--- src/passes/CSE.cpp
+++ src/passes/CSE.cpp
@@ -161,18 +161,18 @@ struct CSE
   }
 
   void doWalkFunction(Function* func) {
-std::cout << "func " << func->name << '\n';
+//std::cout << "func " << func->name << '\n';
     // First scan the code to find all the expressions and basic blocks. This
     // fills in |blocks| and starts to fill in |exprInfos|.
     WalkerPass<
       CFGWalker<CSE, UnifiedExpressionVisitor<CSE>, CSEBasicBlockInfo>>::
       doWalkFunction(func);
-std::cout << "  a\n";
+//std::cout << "  a\n";
 
     Linearize linearize;
     linearize.walk(func->body);
     auto exprInfos = std::move(linearize.exprInfos);
-std::cout << "  b\n";
+//std::cout << "  b\n";
 
     // Do another pass to find repeated expressions. We are looking for complete
     // expressions, including children, that recur, and so it is efficient to do
@@ -300,7 +300,7 @@ if (effects.hasSideEffects() || // TODO: nonremovable?
     }
     // std::cout << "phase 2\n";
 
-std::cout << "  c\n";
+//std::cout << "  c\n";
 
     // We have filled in |exprInfos| with copy information, and we've found at
     // least one relevant copy. We can now apply those copies. We start at the
@@ -315,7 +315,7 @@ std::cout << "  c\n";
     // To see which copies can actually be optimized, we need to see that the
     // first dominates the second.
     cfg::DominationChecker<BasicBlock> dominationChecker(basicBlocks);
-std::cout << "  d\n";
+//std::cout << "  d\n";
 
     Builder builder(*module);
 
@@ -478,7 +478,7 @@ std::cout << "  d\n";
       return;
     }
 
-std::cout << "  e\n";
+//std::cout << "  e\n";
 
     Index z = 0;
     for (auto& info : exprInfos) {
@@ -494,11 +494,11 @@ std::cout << "  e\n";
       z++;
     }
 
-std::cout << "  f\n";
+//std::cout << "  f\n";
 
     // Fix up any nondefaultable locals that we've added.
     TypeUpdating::handleNonDefaultableLocals(func, *getModule());
-std::cout << "  g\n";
+//std::cout << "  g\n";
   }
 
 private:
